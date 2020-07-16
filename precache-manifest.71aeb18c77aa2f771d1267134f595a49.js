2a9df9289c6be2f9849ac99943520b03021bb907
Adrian Chiriac <52508424+chiriacadrian@users.noreply.github.com>
2020-07-16 07:04:45 +0200

    Updates

---
+++ precache-manifest.71aeb18c77aa2f771d1267134f595a49.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba6e39bcf500c547baadd634e4e6d8d",
+    "url": "/breaking-bad-characters/index.html"
+  },
+  {
+    "revision": "581d02dc6c813e3c6bdb",
+    "url": "/breaking-bad-characters/static/css/main.f53ddda0.chunk.css"
+  },
+  {
+    "revision": "a6c4709abac6cf2985cd",
+    "url": "/breaking-bad-characters/static/js/2.07fb14dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-characters/static/js/2.07fb14dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581d02dc6c813e3c6bdb",
+    "url": "/breaking-bad-characters/static/js/main.36a420d4.chunk.js"
+  },
+  {
+    "revision": "8e7d853734082d0c29dc",
+    "url": "/breaking-bad-characters/static/js/runtime-main.45e5277b.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-characters/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-characters/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-characters/static/media/spinner.235b9b3e.gif"
+  }
+]);
